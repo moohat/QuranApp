6a427c19d56d8a761fff77f16617b77c5781152a
moohat <refixzcorp@gmail.com>
2022-07-12 09:45:22 -0700

    get api alquran

--- src/pages/ListAlquran/index.js
+++ src/pages/ListAlquran/index.js
@@ -1,15 +1,35 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React, {useEffect} from 'react';
-import {Axios} from 'axios';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+const baseUrl = 'https://quran-endpoint.vercel.app/quran/1';
+
+const parseArray = listObject => {
+  const data = [];
+  Object.keys(listObject).map(key => {
+    data.push({
+      id: key,
+      data: listObject[key],
+    });
+  });
+  return data;
+};
 
 const ListAlquran = () => {
+  const [ayat, setAyat] = useState('');
   const getData = async () => {
-    const dataAlquran = await fetch('https://quran-endpoint.vercel.app/');
-    const value = await dataAlquran.json();
-    console.log('ini data: ', value);
+    const response = await axios.get(baseUrl);
+    const nama_surat = parseArray(response.data.data.asma.id.short);
+    console.log('response dari server: ', parseArray(nama_surat));
+    // setAyat(nama_surat);
+
+    // nama_surat.map(item => {
+    //   item.short;
+    //   console.log('ini surat', item.short);
+    // });
   };
   useEffect(() => {
     getData();
+    // console.log(ayat);
   }, []);
 
   return (
