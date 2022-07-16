abe64eef88d09a903fda004c1b77eba54fb8abad
moohat <refixzcorp@gmail.com>
2022-07-16 11:58:15 -0700

    play audio, masih bug sih

--- src/App.js
+++ src/App.js
@@ -14,3 +14,78 @@ const App = () => {
 export default App;
 
 const styles = StyleSheet.create({});
+
+// import React, {useEffect, useState} from 'react';
+// import {View, StyleSheet, TouchableOpacity} from 'react-native';
+// import Ionicons from 'react-native-vector-icons/Ionicons';
+
+// var Sound = require('react-native-sound');
+
+// Sound.setCategory('Playback');
+
+// var audio = new Sound(
+//   'http://download.quranicaudio.com/quran/ahmed_ibn_3ali_al-3ajamy/001.mp3',
+//   null,
+//   error => {
+//     if (error) {
+//       console.log('failed to load the sound', error);
+//       return;
+//     }
+//     // if loaded successfully
+//     console.log(
+//       'duration in seconds: ' +
+//         audio.getDuration() +
+//         'number of channels: ' +
+//         audio.getNumberOfChannels(),
+//     );
+//   },
+// );
+// const App = () => {
+//   const [playing, setPlaying] = useState();
+//   useEffect(() => {
+//     audio.setVolume(1);
+//     return () => {
+//       audio.release();
+//     };
+//   }, []);
+//   const playPause = () => {
+//     if (audio.isPlaying()) {
+//       audio.pause();
+//       setPlaying(false);
+//     } else {
+//       setPlaying(true);
+//       audio.play(success => {
+//         if (success) {
+//           setPlaying(false);
+//           console.log('successfully finished playing');
+//         } else {
+//           setPlaying(false);
+//           console.log('playback failed due to audio decoding errors');
+//         }
+//       });
+//     }
+//   };
+//   return (
+//     <View style={styles.container}>
+//       <TouchableOpacity style={styles.playBtn} onPress={playPause}>
+//         <Ionicons
+//           name={playing ? 'ios-pause-outline' : 'ios-play-outline'}
+//           size={36}
+//           color={'#fff'}
+//         />
+//       </TouchableOpacity>
+//     </View>
+//   );
+// };
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     backgroundColor: '#000',
+//   },
+//   playBtn: {
+//     padding: 20,
+//   },
+// });
+// export default App;
--- src/pages/DetailAyat/index.js
+++ src/pages/DetailAyat/index.js
@@ -9,44 +9,108 @@ import {
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 const baseUrl = 'https://quran-endpoint.vercel.app/quran';
-import SoundPlayer from 'react-native-sound-player';
+// import SoundPlayer from 'react-native-sound-player';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
+var Sound = require('react-native-sound');
+Sound.setCategory('Playback');
+
+const audio = new Sound(
+  'http://download.quranicaudio.com/quran/ahmed_ibn_3ali_al-3ajamy/001.mp3',
+  null,
+  error => {
+    if (error) {
+      console.log('failed to load the sound', error);
+      return;
+    }
+    // if loaded successfully
+    console.log(
+      'duration in seconds: ' +
+        audio.getDuration() +
+        'number of channels: ' +
+        audio.getNumberOfChannels(),
+    );
+  },
+);
 const DetailAyat = ({route}) => {
   const surat = route.params;
 
   const [surah, setSurah] = useState([]);
+  const [dataAudio, setDataAudio] = useState('');
+  const [playing, setPlaying] = useState(false);
+
   const getData = async () => {
     const response = await axios.get(`${baseUrl}/${surat.number}`);
-    const surah = response.data.data.ayahs;
+    const surah =  response.data.data.ayahs;
+    const fullRecitation = await response.data.data.recitation.full;
     setSurah(surah);
-    surah.map(item => {
-      return console.log('nama surah dari server: ', item.translation.id);
-      
-    });
+    setDataAudio(fullRecitation);
+    // console.log('ini data audio url: ', surah.audio.url);
+
+    // surah.map(item => {
+    //   // return console.log((audio = new Sound(item.audio.url)));
+    //   setDataAudio(item.audio.url);
+    //   // console.log('ini data audio url: ', item.audio.url);
+    // });
     // console.log('nama ayat: ', typeof surah);
   };
+  console.log('ini data audio: ', dataAudio);
+
+  // console.log('ini data audio dari url: ', dataAudio);
+
   useEffect(() => {
     getData();
+    audio.setVolume(1);
+    return () => {
+      audio.release();
+    };
     // console.log(ayat);
   }, []);
 
+  const playPause = () => {
+    if (audio.isPlaying()) {
+      audio.pause();
+      setPlaying(false);
+    } else {
+      setPlaying(true);
+      audio.play(success => {
+        if (success) {
+          setPlaying(false);
+          console.log('successfully finished playing');
+        } else {
+          setPlaying(false);
+          console.log('playback failed due to audio decoding errors');
+        }
+      });
+    }
+  };
+
   //   console.log(`${baseUrl}/${surat.number}`);
   return (
-    <ScrollView showsVerticalScrollIndicator={false}>
-      {surah.map(item => {
-        return (
-          <View>
-            <Text style={styles.label}> {item.number.insurah}</Text>
+    <>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <TouchableOpacity style={styles.playBtn} onPress={playPause}>
+          <Ionicons
+            name={playing ? 'pause-outline' : 'play-outline'}
+            size={36}
+            color={'red'}
+          />
+        </TouchableOpacity>
+        {surah.map(item => {
+          return (
+            <View>
+              <Text style={styles.label}> {item.number.insurah}</Text>
 
-            <Text style={styles.label}> {item.text.ar}</Text>
-            <Text style={styles.label}> {item.audio.url}</Text>
-              <Button title="Play Audio" />
-            <Text style={styles.label}> {item.text.read}</Text>
-            <Text style={styles.label}> {item.translation.id}</Text>
-          </View>
-        );
-      })}
-    </ScrollView>
+              <Text style={styles.label}> {item.text.ar}</Text>
+              <Text style={styles.label}> {item.audio.url}</Text>
+
+              <Text style={styles.label}> {item.text.read}</Text>
+              <Text style={styles.label}> {item.translation.id}</Text>
+            </View>
+          );
+        })}
+      </ScrollView>
+    </>
   );
 };
 
@@ -56,5 +120,14 @@ const styles = StyleSheet.create({
   label: {
     fontSize: 16,
     fontWeight: 'bold',
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#000',
+  },
+  playBtn: {
+    padding: 20,
   },
 });
